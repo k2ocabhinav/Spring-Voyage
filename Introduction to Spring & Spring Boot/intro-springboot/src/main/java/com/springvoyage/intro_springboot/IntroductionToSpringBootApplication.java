878e37cbde0bb3e4b8bc5a68752b22ab0a815ebf
Abhinav Prasad <k2ocabhinav22@gmail.com>
2024-06-29 11:02:34 +0530

    chore: Add project metadata for Introduction to Spring Boot

---
+++ Introduction to Spring & Spring Boot/intro-springboot/src/main/java/com/springvoyage/intro_springboot/IntroductionToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springvoyage.intro_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+	}
+
+}
