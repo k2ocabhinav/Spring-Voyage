21e21ddfdf650bacb076c0ce677fec129eec4dee
Abhinav Prasad <k2ocabhinav22@gmail.com>
2024-07-07 21:47:51 +0530

    chapter: Spring Voyage - Spring Boot MVC and RESTful API

---
+++ Spring Boot MVC and RESTful APIs/mvc-restful-api/src/main/java/com/springvoyage/mvc_restful_api/SpringBootMvcAndResTfulApIsApplication.java
@@ -0,0 +1,13 @@
+package com.springvoyage.mvc_restful_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcAndResTfulApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcAndResTfulApIsApplication.class, args);
+	}
+
+}
